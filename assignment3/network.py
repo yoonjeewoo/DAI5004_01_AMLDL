df879afcbd8511b4c89e4fb7b2718657c712512c
yoonjeewoo <yoonjeewoo@gmail.com>
2021-10-21 19:56:21 +0900

    third assignment

--- assignment3/network.py
+++ assignment3/network.py
@@ -8,13 +8,11 @@ class LSTM_FC(models.Model):
         self.num_classes = num_classes
         self.max_features = max_features
         self.embedding = layers.Embedding(self.max_features, 128)
-        self.lstm1 = layers.LSTM(64, return_sequences=True)
-        self.lstm2 = layers.LSTM(64)
+        self.lstm = layers.LSTM(64)
         self.fc = layers.Dense(self.num_classes, activation="softmax")
 
     def call(self, x):
         x = self.embedding(x)
-        x = self.lstm1(x)
-        x = self.lstm2(x)
+        x = self.lstm(x)
         x = self.fc(x)
         return x
